ec196bf7e411d8a17bf14707acf0bf53cc6bb96b
half-programmer <Kingxc96@outlook.com>
2016-08-27 10:59:28 +0800

    first commit

---
+++ RegisterHandler.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+import json
+from  BaseHandlerh import BaseHandler
+from Database.tables import Appointment,User
+from Database.tables import Verification
+import random
+from messsage import message
+
+def generate_verification_code(len=6):
+ ''' 随机生成6位的验证码 '''
+ # 注意： 这里我们生成的是0-9的列表，当然你也可以指定这个list，这里很灵活
+ code_list = []
+ for i in range(10): # 0-9数字
+  code_list.append(str(i))
+ myslice = random.sample(code_list, len) # 从list中随机获取6个元素，作为一个片断返回
+ verification_code = ''.join(myslice) # list to string
+ return verification_code
+
+class RegisterHandler(BaseHandler):
+    retjson = {'code': '400', 'contents': 'None'}
+    def post(self):
+        type = self.get_argument('type', default='unsolved')
+        if type == '10001':  # 验证手机号
+            m_phone=self.get_argument('phone')
+            try:
+                user = self.db.query(User).filter(User.phone == m_phone).one()
+                if user:
+                    self.retjson['content'] = u"该手机号已经被注册，请更换手机号或直接登录"
+                    self.retjson['code'] = 10005
+            except:
+                code=generate_verification_code()
+                veri=Verification(
+                    phone=m_phone,
+                    verificationcode=code
+                )
+                self.db.merge(veri)
+                try:
+                    self.db.commit()
+                    self.retjson['code'] = 10004 # success
+                    self.retjson['contents'] = u'手机号验证成功，发送验证码'
+                except:
+                    self.db.rollback()
+                    self.retjson['code'] = 10009  # Request Timeout
+                    self.retjson['contents'] = u'服务器错误'
+                message(code, m_phone)
+        elif type=='10002': #验证验证码
+            m_phone=self.get_argument('phone')
+            code=self.get_argument('code')
+            try:
+               item=self.db.query(Verification).filter(Verification.phone==m_phone).one()
+               if item.verificationcode==code:
+                   self.retjson['code']=10004
+                   self.retjson['contents']=u'验证码验证成功'
+               else:
+                   self.retjson['code']=10006
+                   self.retjson['contents']=u'验证码验证失败'
+            except:
+                self.retjson['code']=10007
+                self.retjson['contents']=u'该手机号码未发送验证码'
+
+        elif type=='10003': #注册详细信息
+            m_password=self.get_argument('password')
+            m_nick_name=self.get_argument('nickName')  # 昵称
+            m_phone=self.get_argument('phone')
+            new_user=User(
+                    password=m_password,
+                    nick_name=m_nick_name,
+                    real_name='',
+                    level=1,  #  新用户注册默认level为1
+                    location='',
+                    birthday='',
+                    phone=m_phone,
+                    regist_time=''
+            )
+            try:
+                same_nickname_user = self.db.query(User).filter(User.nick_name == m_nick_name).one()
+                if same_nickname_user:  # 该昵称已被使用
+                    self.retjson['code'] = 10008  # Request Timeout
+                    self.retjson['contents'] =u'该昵称已被使用，请更换昵称'
+            except: # 手机号和昵称皆没有被注册过
+                    self.db.merge(new_user)
+                    try:
+                        self.db.commit()
+                        self.retjson['code'] = 10004  # success
+                        self.retjson['contents'] = u'注册成功'
+                    except:
+                        self.db.rollback()
+                        self.retjson['code'] = 10009  # Request Timeout
+                        self.retjson['contents'] = u'Some errors when commit to database, please try again'
+        self.write(json.dumps(self.retjson, ensure_ascii=False, indent=2))
+
---
+++ loginHandler.py
@@ -0,0 +1,64 @@
+# coding=utf-8
+import json
+
+from BaseHandlerh import BaseHandler
+from Database.tables import Appointment, User
+
+
+class LoginHandler(BaseHandler):
+
+    retjson = {'code': '400', 'contents': u'未处理 ', 'Code': ''}
+    def post(self):
+        askcode = self.get_argument('askCode')
+        m_phone = self.get_argument('phone')
+        if askcode == '10106':  # 手动登录
+            m_password = self.get_argument('password')
+            if not m_phone or not m_password:
+                LoginHandler.retjson['code'] = 400
+                LoginHandler.retjson['contents'] = u'用户名密码不能为空'
+
+        # 防止重复注册
+            else:
+                try:
+                    user = self.db.query(User).filter(User.phone == m_phone).one()
+                    if user:  # 用户存在
+                        password = user.password
+                        if m_password == password:  # 密码正确
+                            self.retjson['code'] = 200
+                            retdata = []
+                            user_model = dict(
+                                id=user.userID,
+                                phone=user.phone,
+                                nick_name=user.nick_name,
+                                avatar="图片链接",
+                            )
+                            data = dict(
+                            askCode="10106",
+                            authKey="待生成",
+                            userModer=user_model,
+                            daohanglan="约拍首页顶部滑动图片,应设置与本地对比或增加一特定链接，图片未更新时应使用本地缓存",
+                            photoList="摄影师榜前十名,每人是一组数据,用python字典存,返回后可用JSON解析",
+                            modelList="模特榜前十名，每人是一组数据,用python字典存,返回后可用JSON解析",
+                            headImage="用户头像url"
+                            )
+                            retdata.append(data)
+                            LoginHandler.retjson['Code'] = 10101
+                            LoginHandler.retjson['contents'] = retdata
+
+                        else:
+                            LoginHandler.retjson['Code'] = 10104  # 密码错误
+                    else:  # 用户不存在
+                        LoginHandler. retjson['Code'] = 10103
+                except Exception, e:  # 还没有注册
+                    print "异常："
+                    print e
+                    self.retjson['code'] = 400
+                    self.retjson['Code'] = u'该用户名不存在'
+        elif askcode == '10105':  # 自动登录
+            authcode = self.get_argument("authcode")  # 授权码
+        else:
+            LoginHandler.retjson['data'] = "去你妈了个吧"
+        self.write(json.dumps(LoginHandler.retjson, ensure_ascii=False, indent=2))
+
+
+
