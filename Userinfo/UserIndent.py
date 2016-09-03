b9349b3aa5dd31b51b6ebb36f56cdc9c5048d5fe
lanway <115564098@qq.com>
2016-09-03 22:18:59 +0800

    lll

--- Userinfo/UserIndent.py
+++ Userinfo/UserIndent.py
@@ -86,14 +86,20 @@ class UserIndent(BaseHandler):
 
         for ap_e_entry in ap_e_entrys:
             ap_id = ap_e_entry.AEapid
-            ap_e_info = self.db.query(Appointment).filter(Appointment.APid == ap_id,int(Appointment.APstatus) == int(number)).all()
+            try :
+               ap_e_info = self.db.query(Appointment).filter(Appointment.APid == ap_id,Appointment.APstatus == number).all()
+            except Exception,e:
+                print e
             if ap_e_info:
                 ret_e_appointment.append(APmodelHandler.ap_Model_simply_one(ap_e_info[0]))
         return ret_e_appointment
 
     def get_my_appointment(self,u_id,number):
         ret_my_appointment =[]
-        ap_my_entrys = self.db.query(Appointment).filter(Appointment.APsponsorid == u_id,int(Appointment.APstatus) == int(number)).all()
+        try:
+            ap_my_entrys = self.db.query(Appointment).filter(Appointment.APsponsorid == u_id,Appointment.APstatus == number).all()
+        except Exception,e:
+            print e
         ret_my_appointment = APmodelHandler.ap_Model_simply(ap_my_entrys, ret_my_appointment)
         return ret_my_appointment
 
