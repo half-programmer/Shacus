7cae37047bf042f91c25b0d25dbb49b582debad8
lanway <115564098@qq.com>
2016-09-06 08:51:26 +0800

    lll

--- Course/Chomepage.py
+++ Course/Chomepage.py
@@ -9,7 +9,17 @@ from Userinfo import Ufuncs
 
 
 class Chomepage(BaseHandler):# 教程首页
+    retjson ={ "code": '','contents':''}
 
     def post(self):
+        u_id = self.get_argument('uid')
+        u_authkey = self.get_argument('authkey')
         ufuncs = Ufuncs()
-        if ufuncs.judge_user_valid()
+        if ufuncs.judge_user_valid(u_id,u_authkey):
+            type = self.get_argument('type')
+            if type == '11001':  #查看教程首页
+
+
+        else :
+            self.retjson['contents'] = '用户授权码不正确'
+            self.retjson['code'] = '11000'
