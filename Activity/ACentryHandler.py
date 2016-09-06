1a54268515d74a65a43faeb7c771944253d6fe04
wjl <ra7more@outlook.com>
2016-09-06 18:58:37 +0800

    sdfsd

--- Activity/ACentryHandler.py
+++ Activity/ACentryHandler.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 import datetime
 
+from Userinfo.Ufuncs import Ufuncs
+
 __anthor__="wjl"
 import json
 
@@ -13,7 +15,27 @@ from Database.tables import ActivityEntry, Activity,ActivityLike
 class AskEntry(BaseHandler): #活动报名点赞表相关操作
     retjson = {'code': 200, 'contents': 'none'}
     retdata = []  # list array
+
+    def db_commit(self, name):
+        try:
+            self.prase_success(name)
+        except Exception, e:
+            self.db_commit_fail(e)
+
+    def prase_success(self, name):
+        self.db.commit()
+        self.retjson['code'] = '10381'
+        self.retjson['contents'] = name
+
+    def db_commit_fail(self, e):
+        print e
+        self.retjson['code'] = '10380'
+        self.retjson['contents'] = r'数据库提交失败'
+
     def post(self):
+
+
+
         type=self.get_argument("type",default="null")
         if type=="10309": #查看已报名活动
             m_id=self.get_argument("registerid",default = "null")
@@ -60,46 +82,69 @@ class AskEntry(BaseHandler): #活动报名点赞表相关操作
                     print e
 
 
-
-        elif type=='10311':#活动点赞
-            m_ACLacid=self.get_argument("ACLacid",default="null")
-            m_ACLudi=self.get_argument("ACLuid",default='null')
-            try:
-                data=self.db.query(ActivityLike).filter(Activity.ACLacid==m_ACLacid,ActivityLike.ACLuid==m_ACLudi,ActivityLike.ACLvalid==False).one()
-            # if data:
-            #     self.retdata['contents']='你已经点赞过此'
-                new_ACLide=ActivityLike(
-                   ACLacid=m_ACLacid,
-                    ACLuid=m_ACLudi,
-                    AClvalid=1,
-                    )
-                self.db.merge(new_ACLide)
-                self.db.commit()
-                self.retjson['code'] = '10311'
-                self.retjson['contents'] = '点赞成功'
-            except Exception,e:
-                print e
-                self.retjson['code']=10312
-                self.retjson['contents']='点赞未成功'
-        elif type=='10313':
-            m_ACLacid = self.get_argument("ACLacid", default="null")
-            m_ACLudi = self.get_argument("ACLuid", default='null')
-            try:
-                data = self.db.query(ActivityLike).filter(Activity.ACLacid == m_ACLacid, ActivityLike.ACLuid == m_ACLudi,ActivityLike.ACLvalid == True).one()
-                data.ACLvalid = False
-                self.retjson['code'] = '10313'
-                self.retjson['contents'] = '取消点赞成功'
-            except Exception,e:
-                print e
-                self.retjson['code']='10314'
-                self.retjson['contents']='取消点赞失败'
-
-
-
-
-
-
-
+        elif type=='10361'or type=='10362':
+       # elif type=='10311':#活动点赞
+            uid = self.get_argument('uid')
+            u_authkey = self.get_argument('authkey')
+            m_ACLacid=self.get_argument("aclacid",default="null")#活动的id
+            ufunc = Ufuncs()
+            if ufunc.judge_user_valid(uid, u_authkey):
+                try:
+                    print m_ACLacid
+                    data=self.db.query(Activity).filter(Activity.ACid==m_ACLacid).one()
+                    if data.ACvalid == 0:
+                        self.retjson['contents']='活动已经失效'
+                    else:
+                        try:#是否已经点过赞
+                            once_liked = self.db.query(ActivityLike).filter(ActivityLike.ACLacid == m_ACLacid,
+                                                                           ActivityLike.ACLuid == uid).one()
+                            if once_liked:
+                                if once_liked.ACLvalid == 1:  # 已经赞过
+                                    if type == '10361':  # 对活动进行点赞
+                                        self.retjson['code'] = '10385'
+                                        self.retjson['contents'] = r'已点过赞'
+                                    elif type == '10362':  # 取消赞
+                                        once_liked.ACLvalid = 0
+                                        try:
+                                            Activity.AClikenumber -= 1
+                                            self.db.commit()
+                                            self.retjson['code'] = '10386'
+                                            self.retjson['contents'] = r'取消赞成功'
+                                        except Exception, e:
+                                            self.db_commit_fail(e)
+                                else:  # 曾经点过赞，但是已经取消
+                                    if type == '10361':
+                                        once_liked.ACLvalid = 1
+                                        Activity.AClikenumber += 1
+                                        self.db_commit(r'点赞成功')
+                                        self.retjson['code'] = '10387'
+                                        self.retjson['contents'] = r'重新点赞成功'
+                                    elif type == '10362':
+                                        self.retjson['code'] = '10386'
+                                        self.retjson['contents'] = r'用户已取消赞！'
+                        except Exception, e:  # 未对这个操作过
+                              print e
+                              if type == '10362':
+                                    self.retjson['code'] = '10390'
+                                    self.retjson['contents'] = r'用户未赞过此约拍！'
+                              elif type == '10361':
+                                    new_Aclike = ActivityLike(
+                                        ACLacid=m_ACLacid,
+                                        ACLuid=uid,
+                                        ACLvalid=1,
+                                    )
+                                    Activity.AClikenumber += 1
+                                    self.db.merge(new_Aclike)
+                                    self.db_commit(r'点赞成功')
+                                    self.retjson['code'] = '10387'
+                                    self.retjson['contents'] = '重新点赞成功'
+                except Exception, e:
+                    print e
+                    self.retjson['code'] = '10391'
+                    self.retjson['contents'] = '该活动不存在'
+            else:
+                    self.retjson['code'] = '10392'
+                    self.retjson['contents'] = '用户认证失败'
 
         self.write(json.dumps(self.retjson, ensure_ascii=False, indent=2))  # 返回中文
 
