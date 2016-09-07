790866d63eb54d93bf930bfa9d505f438e983ee8
wjl <ra7more@outlook.com>
2016-09-07 14:55:03 +0800

    auth_key

--- Activity/ACaskHandler.py
+++ Activity/ACaskHandler.py
@@ -18,61 +18,79 @@ from Userinfo.Ufuncs import Ufuncs
 class AskActivity(BaseHandler): #关于用户的一系列活动
     retjson = {'code': '200', 'contents': 'none'}
     def post(self):
+        u_auth_key = self.get_argument('authkey')
+        request_type = self.get_argument('type')
+        u_id = self.get_argument('uid')
         retdata = []  # list array
         type = self.get_argument('type', default='unsolved')
         if type == '10303':  # 1.查看所有活动
-            try:
-              #  data = self.db.query(Activity).all()
-                data=self.db.query(Activity).order_by(desc(Activity.ACcreateT)).all()
-                length=len(data)
-                print length
-                if length < 10:
-                    for i in range(length):
 
-                        #dataimage = self.db.query(ActivityImage).filter(data[i].ACid == ActivityImage.ACLacid).one()
-                        datauser=self.db.query(User).filter(data[i].ACsponsorid==User.Uid).one()
-                        ACFunction.Acresponse(data[i],datauser,retdata)
-                        self.retjson['code']=10303
-                        self.retjson['contents']=retdata
+                u_id = self.get_argument('uid')
+                u_auth_key = self.get_argument('authkey')
+                ufuncs = Ufuncs()  # 判断用户权限
+                if ufuncs.judge_user_valid(u_id, u_auth_key):  # 用户认证成功
+                    try:
+                        data=self.db.query(Activity).order_by(desc(Activity.ACcreateT)).all()
+                        length=len(data)
+                        print length
+                        if length < 10:
+                            for i in range(length):
+
+                            #dataimage = self.db.query(ActivityImage).filter(data[i].ACid == ActivityImage.ACLacid).one()
+                                datauser=self.db.query(User).filter(data[i].ACsponsorid==User.Uid).one()
+                                ACFunction.Acresponse(data[i],datauser,retdata,u_id)
+                                self.retjson['code']='10303'
+                                self.retjson['contents']=retdata
+                        else:
+                            for item in range(0,10):
+                             #dataimage = self.db.query(ActivityImage).filter(data[item].ACid == ActivityImage.ACLacid).one()
+                             datauser = self.db.query(User).filter(data[item].ACsponsorid == User.Uid).one()
+                             ACFunction.Acresponse(data[item],datauser,retdata,u_id)
+                             self.retjson['code'] = '10303'
+                             self.retjson['contents'] = retdata
+                    except Exception, e:
+                        print e
+                        self.retjson['code'] = '200'
+                        self.retjson['contents'] = 'there is no activity'
                 else:
-                    for item in range(0,10):
+                    self.retjson['code'] = '10373'
+                    self.retjson['contents'] = '认证未通过'
+
+        elif type =='10304':    #后来的5个
+            u_id = self.get_argument('uid')
+            u_auth_key = self.get_argument('authkey')
+            ufuncs = Ufuncs()  # 判断用户权限
+            if ufuncs.judge_user_valid(u_id, u_auth_key):  # 用户认证成功
+                try:
+                    acsended=self.get_argument('acsended')
+                    Acsended=int(acsended)
+                    data= data=self.db.query(Activity).order_by(desc(Activity.ACcreateT)).all()
+                    length = len(data)
+                    print length
+                    m_length=length-Acsended
+                    print m_length
+                    if (m_length<5):
+                        for i in range(Acsended,length):
+                            #dataimage = self.db.query(ActivityImage).filter(data[i].ACid == ActivityImage.ACLacid).one()
+                            datauser = self.db.query(User).filter(data[i].ACsponsorid == User.Uid).one()
+                            ACFunction.Acresponse(data[i],retdata,u_id)
+                            self.retjson['code'] = 10304
+                            self.retjson['contents'] =retdata
+                    else:
+                        for item in range(Acsended,acsended+6):
                             #dataimage = self.db.query(ActivityImage).filter(data[item].ACid == ActivityImage.ACLacid).one()
-                        datauser = self.db.query(User).filter(data[item].ACsponsorid == User.Uid).one()
-                        ACFunction.Acresponse(data[item],datauser,retdata)
-                        self.retjson['code'] = 10303
-                        self.retjson['contents'] = retdata
-            except Exception, e:
+                            datauser = self.db.query(User).filter(data[item].ACsponsorid == User.Uid).one()
+                            ACFunction.Acresponse(data[item],retdata,u_id)
+                            self.retjson['code'] = 10304
+                            self.retjson['contents'] = retdata
+
+                except Exception,e:
                     print e
                     self.retjson['code'] = 200
                     self.retjson['contents'] = 'there is no activity'
-        elif type =='10304':    #后来的5个
-            try:
-                acsended=self.get_argument('acsended')
-                Acsended=int(acsended)
-                data= data=self.db.query(Activity).order_by(desc(Activity.ACcreateT)).all()
-                length = len(data)
-                print length
-                m_length=length-Acsended
-                print m_length
-                if (m_length<5):
-                    for i in range(Acsended,length):
-                        #dataimage = self.db.query(ActivityImage).filter(data[i].ACid == ActivityImage.ACLacid).one()
-                        datauser = self.db.query(User).filter(data[i].ACsponsorid == User.Uid).one()
-                        ACFunction.Acresponse(data[i],retdata)
-                        self.retjson['code'] = 10304
-                        self.retjson['contents'] =retdata
-                else:
-                    for item in range(Acsended,acsended+6):
-                        #dataimage = self.db.query(ActivityImage).filter(data[item].ACid == ActivityImage.ACLacid).one()
-                        datauser = self.db.query(User).filter(data[item].ACsponsorid == User.Uid).one()
-                        ACFunction.Acresponse(data[item],retdata)
-                        self.retjson['code'] = 10304
-                        self.retjson['contents'] = retdata
-
-            except Exception,e:
-                print e
-                self.retjson['code'] = 200
-                self.retjson['contents'] = 'there is no activity'
+            else:
+                self.retjson['code'] = '10373'
+                self.retjson['contents'] = '认证未通过'
 
 
 
@@ -96,6 +114,7 @@ class AskActivity(BaseHandler): #关于用户的一系列活动
                  issponsor= 1
              else:
                  issponsor= 0
+
 
 
 
