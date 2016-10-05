13d331b561ff17c018871b3c127111fce78ced90
lanway <115564098@qq.com>
2016-10-05 23:46:21 +0800

    sign

--- Wechatserver/WgetSign.py
+++ Wechatserver/WgetSign.py
@@ -21,11 +21,11 @@ class WgetSign(BaseHandler):
 
         type = self.get_argument('type')
         appsecret = self.get_argument("appsecret")
-        if appsecret == conf.appsecret and type == 'getSignature':
+        if appsecret == conf.appsecret:
             ip = self.request.remote_ip
             #url = 'http://%s:80/WgetSign.html'%ip
             #url = 'http://e1b3b8b0.ngrok.io/WgetSign.html'
-            url = 'http://shacuswechat.tunnel.2bdata.com/'
+            url = 'http://shacuswechat.tunnel.2bdata.com/{}'.format(type)
             wjs = WJS(url)
             ret = wjs.sign()
             callback = self.get_argument("jsoncallback")
