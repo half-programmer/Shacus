2863745f71b9c4883479a5fdd21292ce18cfc80c
lanway <115564098@qq.com>
2016-10-05 15:36:20 +0800

    签名更改

--- Wechatserver/WgetSign.py
+++ Wechatserver/WgetSign.py
@@ -10,19 +10,21 @@ import json
 from BaseHandlerh import BaseHandler
 
 from WJS import WJS
+from Wconf import Wconf
 class WgetSign(BaseHandler):
 
     def get(self):
         ret = []
+        conf = Wconf.conf
 
         type = self.get_argument('type')
-
-        if type == '20001':
+        appsecret = self.get_argument("appsecret")
+        if appsecret == conf.appsecret and type == 'getSignature':
             ip = self.request.remote_ip
             url = 'http://%s:80/WgetSign.html'%ip
             wjs = WJS(url)
-            self.ret = wjs.sign()
-        self.write(json.dumps(self.ret,ensure_ascii=False, indent=2))
+            ret = wjs.sign()
+        self.write(json.dumps(ret,ensure_ascii=False, indent=2))
 
 
 
